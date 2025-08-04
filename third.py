cfe8f2eb85e3d5f9bee265c84f3506705c982bbe
MouliMohan <msadaram@gitam.in>
2025-08-04 15:15:07 +0530

    Create third.py

---
+++ third.py
@@ -0,0 +1,13 @@
+import math
+
+with open("input.txt", "r") as file:
+    a, b, c = map(float, file.readline().split())
+
+D = b**2 - 4*a*c
+
+if D >= 0:
+    root1 = (-b + math.sqrt(D)) / (2*a)
+    root2 = (-b - math.sqrt(D)) / (2*a)
+    print("Roots are:", root1, root2)
+else:
+    print("No real roots")
